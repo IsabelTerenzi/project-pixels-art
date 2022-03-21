cd7f0767671d4346ada6fa82de83be77a2f05039
Isabel Terenzi <bel.terenzi@gmail.com>
2022-03-21 15:53:34 -0300

    requisito 4

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let i = 0;
+for (i = 0; i <= 24; i += 1){
+let quadradinho = document.createElement('div');
+quadradinho.classList = ('pixel');
+document.getElementById('pixel-board').appendChild(quadradinho);
+}
