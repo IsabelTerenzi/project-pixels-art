661cdb199425e3d8af58eda9380e384e23ef211b
Isabel Terenzi <bel.terenzi@gmail.com>
2022-03-21 19:28:05 -0300

    adiciona requisitos

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-
 let i = 0;
-for (i = 0; i <= 24; i += 1){
-let quadradinho = document.createElement('div');
-quadradinho.classList = ('pixel');
-document.getElementById('pixel-board').appendChild(quadradinho);
+for (i = 0; i <= 24; i += 1) {
+  const quadradinho = document.createElement('div');
+  quadradinho.classList = 'pixel';
+  document.getElementById('pixel-board').appendChild(quadradinho);
+
 }
+
+  function limpaTela () {
+  let quadradinho = document.querySelectorAll('.pixel');
+   for (let i = 0; i < quadradinho.length; i += 1){
+   quadradinho[i].style.backgroundColor = 'blue';
+  }
+}
+let limpar = document.querySelectorAll('#clear-board')[0];
+limpar.addEventListener('click', limpaTela);
+
+const corPixel = document.getElementById('pixel-board');
+const black = document.getElementById('black');
+const red = document.getElementById('red');
+const blue = document.getElementById('blue');
+const green = document.getElementById('green');
+
+function pegarCor(event){
+  const selecionaCor = document.querySelectorAll('.selected');
+  selecionaCor.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+black.addEventListener('click', pegarCor);
+red.addEventListener('click', pegarCor);
+blue.addEventListener('click', pegarCor);
+green.addEventListener('click', pegarCor);
+
+function transformaCor (event){
+const transformacao = event;
+const selecionaCor = document.getElementsByClassName('selected');
+transformacao.target.style.backgroundColor = selecionaCor.id;
+}
+corPixel.addEventListener('click', transformaCor);
+
+
