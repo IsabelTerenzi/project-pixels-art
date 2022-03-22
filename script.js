6e448890dea4bf7f8fdbf173d2bdc21a38141139
Isabel Terenzi <bel.terenzi@gmail.com>
2022-03-21 22:27:54 -0300

    requisitos

--- script.js
+++ script.js
@@ -5,36 +5,35 @@ for (i = 0; i < 25; i += 1) {
   document.getElementById('pixel-board').appendChild(linha);
 }
 
-
 function limpaTela() {
-  let quadradinho = document.querySelectorAll('.pixel');
+  const quadradinho = document.querySelectorAll('.pixel');
   for (let i = 0; i < quadradinho.length; i += 1) {
     quadradinho[i].style.backgroundColor = 'white';
   }
 }
-let limpar = document.querySelectorAll('#clear-board')[0];
+const limpar = document.querySelectorAll('#clear-board')[0];
 limpar.addEventListener('click', limpaTela);
 
-const corPixel = document.getElementById('pixel-board');
-const black = document.getElementById('black');
-const red = document.getElementById('red');
-const blue = document.getElementById('blue');
-const green = document.getElementById('green');
+let corPixel = document.querySelector('#pixel-board');
+let preto = document.getElementById('black');
+let vermelho = document.getElementById('red');
+let azul = document.getElementById('blue');
+let verde = document.getElementById('green');
 
 function pegarCor(event) {
-  const selecionaCor = document.querySelectorAll('.selected');
+  const selecionaCor = document.querySelector('.selected');
   selecionaCor.classList.remove('selected');
   event.target.classList.add('selected');
 }
 
-black.addEventListener('click', pegarCor);
-red.addEventListener('click', pegarCor);
-blue.addEventListener('click', pegarCor);
-green.addEventListener('click', pegarCor);
+preto.addEventListener('click', pegarCor);
+vermelho.addEventListener('click', pegarCor);
+azul.addEventListener('click', pegarCor);
+verde.addEventListener('click', pegarCor);
 
 function transformaCor(event) {
   const transformacao = event;
-  const selecionaCor = document.getElementsByClassName('selected');
+  const selecionaCor = document.querySelector('.selected');
   transformacao.target.style.backgroundColor = selecionaCor.id;
 }
 corPixel.addEventListener('click', transformaCor);
